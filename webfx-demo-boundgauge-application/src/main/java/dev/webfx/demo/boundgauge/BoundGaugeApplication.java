dce1d2138ac1a0b1da679c469b102c87c216a4de
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-17 08:50:58 +0100

    Introduced more layout variables and centralised all layoutInArea() calls in one go

--- webfx-demo-boundgauge-application/src/main/java/dev/webfx/demo/boundgauge/BoundGaugeApplication.java
+++ webfx-demo-boundgauge-application/src/main/java/dev/webfx/demo/boundgauge/BoundGaugeApplication.java
@@ -4,13 +4,15 @@ import eu.hansolo.medusa.Gauge;
 import eu.hansolo.medusa.GaugeBuilder;
 import javafx.application.Application;
 import javafx.geometry.HPos;
-import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Slider;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -33,39 +35,47 @@ public final class BoundGaugeApplication extends Application {
         Slider valueSlider = new Slider(0, 100, 50);
         Slider thresholdSlider = new Slider(0, 100, 85);
 
-        // This improves slider performance on low devices (reduce click target possibilities)
-        gauge.setMouseTransparent(true);
-
         // Binding Gauge value and threshold with sliders
         gauge.valueProperty().bind(valueSlider.valueProperty());
         gauge.tresholdProperty().bind(thresholdSlider.valueProperty());
 
+        // This improves slider performance on low devices (reduce click target possibilities)
+        gauge.setMouseTransparent(true);
+        Rectangle clip = new Rectangle();
+        gauge.setClip(clip);
+
         Pane root = new Pane(gauge, valueSlider, thresholdSlider) {
             @Override
             public void layoutChildren() {
-                double width = getWidth(), height = getHeight();
-                Orientation sliderOrientation;
-                if (width >= height) {
-                    sliderOrientation = Orientation.VERTICAL;
-                    double sliderAreaWidth = Math.max(40, (width - height) / 2), sliderAreaHeight = 0.8 * height, sliderY = (height - sliderAreaHeight) / 2;
-                    layoutInArea(gauge, sliderAreaWidth, 0, width - 2 * sliderAreaWidth, height, 0, HPos.CENTER, VPos.CENTER);
-                    layoutInArea(valueSlider, 0, sliderY, sliderAreaWidth, sliderAreaHeight, 0, HPos.CENTER, VPos.CENTER);
-                    layoutInArea(thresholdSlider, width - sliderAreaWidth, sliderY, sliderAreaWidth, sliderAreaHeight, 0, HPos.CENTER, VPos.CENTER);
+                double w = getWidth(), h = getHeight(), vsx, vsy, tsx, tsy, sw, sh, gx, gy, gw, gh;
+                Orientation so;
+                if (w >= h) {
+                    so = Orientation.VERTICAL; // Sliders orientation
+                    sw = Math.max(40, (w - h) / 2); sh = 0.8 * h; // Sliders area width & height
+                    vsx = 0; vsy = (h - sh) / 2; // Value slider x & y
+                    tsx = w - sw; tsy = vsy; // Threshold slider x & y
+                    gx = sw; gy = 0; // Gauge x & y
+                    gw = w - 2 * sw; gh = h; // Gauge width & height
                 } else {
-                    sliderOrientation = Orientation.HORIZONTAL;
-                    double sliderAreaHeight = Math.max(40, (height - width) / 2), sliderAreaWidth = 0.8 * width, sliderX = (width - sliderAreaWidth) / 2;
-                    layoutInArea(gauge, 0, sliderAreaHeight, width, height - 2 * sliderAreaHeight, 0, HPos.CENTER, VPos.CENTER);
-                    layoutInArea(valueSlider, sliderX, 0, sliderAreaWidth, sliderAreaHeight, 0, HPos.CENTER, VPos.CENTER);
-                    layoutInArea(thresholdSlider, sliderX, height - sliderAreaHeight, sliderAreaWidth, sliderAreaHeight, 0, HPos.CENTER, VPos.CENTER);
+                    so = Orientation.HORIZONTAL;  // Sliders orientation
+                    sh = Math.max(40, (h - w) / 2); sw = 0.8 * w; // Slider areas width & height
+                    vsx = (w - sw) / 2; vsy = 0; // Value Slider x & y
+                    tsx = vsx; tsy = h - sh; // Threshold slider x & y
+                    gx = 0; gy = sh; // Gauge x & y
+                    gw = w; gh = h - 2 * sh; // Gauge width & height
                 }
-                valueSlider.setOrientation(sliderOrientation);
-                thresholdSlider.setOrientation(sliderOrientation);
+                valueSlider.setOrientation(so);
+                thresholdSlider.setOrientation(so);
+                clip.setWidth(gw); clip.setHeight(gh);
+                layoutInArea(gauge, gx, gy, gw, gh, 0, HPos.CENTER, VPos.CENTER);
+                layoutInArea(valueSlider, vsx, vsy, sw, sh, 0, HPos.CENTER, VPos.CENTER);
+                layoutInArea(thresholdSlider, tsx, tsy, sw, sh, 0, HPos.CENTER, VPos.CENTER);
             }
         };
-        root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        root.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
         Scene scene = new Scene(root,800, 600);
         stage.setScene(scene);
-        stage.setTitle("Modern Gauge");
+        stage.setTitle("Bound Gauge");
         stage.show();
     }
 
